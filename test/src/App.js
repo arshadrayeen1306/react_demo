b20dc55737e5ccacb7c890e2033e7ae66a68b448
Abhi <69311628+abhi8233@users.noreply.github.com>
2020-08-26 09:44:14 +0530

    Test

--- test/src/App.js
+++ test/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Button, Modal} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      show: false
+    }
+  }
+
+  handelModal1(){
+    this.setState({ show: !this.state.show })
+  }
+
+  handelModal2(){
+    this.setState({ show: !this.state.show })
+  }
+
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button onClick={()=>this.handelModal1()} variant="primary" >All Contacts</Button>
+        <Modal show={this.state.show} onHide={()=>this.handelModal1()}>
+          <Modal.Header closeButton>
+            Modal 1
+          </Modal.Header>
+          <Modal.Body>
+              asdasdad
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={()=>this.handelModal1()}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+		    
+        <Button onClick={()=>this.handelModal2()} variant="success">US Contacts</Button>
+
+        <Modal show={this.state.show} onHide={()=>this.handelModal2()}>
+          <Modal.Header closeButton>
+            Modal 2
+          </Modal.Header>
+          <Modal.Body>
+              asdasdad
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={()=>this.handelModal2()}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+        
       </header>
     </div>
   );
+}
 }
 
 export default App;
---
+++ test/src/FirstModal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class FirstModal extends React.Component{
+
+ 
+
+  return (
+       sdsdf
+  );
+}
+
+export default FirstModal;
